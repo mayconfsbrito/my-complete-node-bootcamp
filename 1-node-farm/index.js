f216a645c2f86172b8542d2bd5ac2e9a56f7ddc6
Maycon Brito <mayconfsbrito@gmail.com>
2019-12-26 13:48:29 -0300

    NPM - Start and install nodemon and slugfy

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const replaceTemplate = require('./modules/replaceTemplate')
+const replaceTemplate = require('./modules/replaceTemplate');
 
 //////////////////////////////////////////////////////
 // FILES
