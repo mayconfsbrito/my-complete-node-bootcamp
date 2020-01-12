b417a5b3507d0ec4e6e0d22f4d5c6c7e28747942
Maycon Brito <mayconfsbrito@gmail.com>
2020-01-12 16:17:25 -0300

    Promises - Removing callback hell and implemented the chain of THEN methods

--- 3-Asynchronous-js/index.js
+++ 3-Asynchronous-js/index.js
@@ -1,19 +1,39 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-  console.log(`Breed: ${data}`);
+const readFilePro = file => {
+  //Will return a promise that will read the file and return its content
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      //The function REJECT will pass the error to the CATCH handler to the promise below
+      if (err) reject('I could not find that file 😢!');
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    // then get the promise, but handle just fullfiled promieses
-    .then(res => {
-      fs.writeFile('dog-img.txt', res.body.message, err => {
-        console.log('Random dog image saved to file!');
-      });
-    })
-    // catch the error object
-    .catch(err => {
-      return console.log(err.message);
+      //This function will pass this value to the promise function below in the THAN handler
+      resolve(data);
     });
-});
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject('I could not write the file!');
+      resolve('success');
+    });
+  });
+};
+
+//Run the function above and run the promise returned
+//To chain THEN methods its necessary to return a promise before calling each of them
+readFilePro(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then(res => {
+    console.log(res.body.message);
+    return writeFilePro('dog-img.txt', res.body.message);
+  })
+  .catch(err => {
+    return console.log(err);
+  });
