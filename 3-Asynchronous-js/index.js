10d0a9b750c64e560d12b6c9f9a254aff9f2be08
Maycon Brito <mayconfsbrito@gmail.com>
2020-01-12 15:53:44 -0300

    Promises - then and catch

--- 3-Asynchronous-js/index.js
+++ 3-Asynchronous-js/index.js
@@ -6,12 +6,14 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
 
   superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
-      console.log(res.body);
-
+    // then get the promise, but handle just fullfiled promieses
+    .then(res => {
       fs.writeFile('dog-img.txt', res.body.message, err => {
         console.log('Random dog image saved to file!');
       });
+    })
+    // catch the error object
+    .catch(err => {
+      return console.log(err.message);
     });
 });
