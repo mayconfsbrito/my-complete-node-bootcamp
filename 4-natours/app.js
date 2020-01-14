96a671c2355a25924f6824959b76e166c6acb9da
Maycon Brito <mayconfsbrito@gmail.com>
2020-01-13 22:22:41 -0300

    Created get and post requests for tour resources

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,15 +1,38 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+const filePathTours = `${__dirname}/dev-data/data/tours-simple.json`;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+// Middleware to help to read the request object on the post requests
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(filePathTours));
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(filePathTours, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  });
 });
 
 const port = 3000;
