093f6f4d30836bd5982b0abe8b972cf300d9708e
Maycon Brito <mayconfsbrito@gmail.com>
2020-01-26 15:46:57 -0300

    Database Connection and a draft to save tour resource in the database

--- 4-natours/server.js
+++ 4-natours/server.js
@@ -1,7 +1,60 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+// It's a convetion to use uppercase in model name and variables.
+// This is why this Tour variable has the capital T name
+const Tour = mongoose.model('Tour', tourSchema);
+
+// Create an instance of Tour object
+const testTour = new Tour({
+  name: 'The Parker Camper',
+  price: 497
+});
+
+// Save this tour instance in the database and print a log in the promise
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR!', err);
+  });
+
 /**
  * Start Server
  */
